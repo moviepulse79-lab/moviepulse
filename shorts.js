7c585ca409cb2b9f033dd4317d12cfc9a30b9548
MOVIE PULSE <moviepulse79@gmail.com>
2026-07-31 18:57:27 +0200

    Add shorts.js with movie details and TikTok links

---
+++ shorts.js
@@ -0,0 +1,23 @@
+const shorts = [
+
+    {
+        title: "Children of Blood and Bone",
+        description: "Latest MoviePulse edit and movie update.",
+        tiktok: "https://www.tiktok.com/@moviepulse_247/video/7668646314795732231"
+    },
+
+
+    {
+        title: "MayDAY2026",
+        description: "A look at one of the most anticipated movies.",
+        tiktok: "https://www.tiktok.com/@moviepulse_247/video/7663152329075592455"
+    },
+
+
+    {
+        title: "SPIDER-MAN:Brand new day",
+        description: "Daily movie news and cinematic moments.",
+        tiktok: "https://www.tiktok.com/@moviepulse_247/video/7663518101870349575"
+    }
+
+];
