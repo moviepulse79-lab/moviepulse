fcdd4a65f32a9737744687142a446168276ab8e7
MOVIE PULSE <moviepulse79@gmail.com>
2026-07-31 19:46:18 +0200

    Update shorts.js

--- shorts.js
+++ shorts.js
@@ -28,5 +28,20 @@ const shorts = [
         title: "Priyanka chopra in her Era",
         description: "Daily movie news and cinematic moments.",
         tiktok: "https://www.tiktok.com/@moviepulse_247/video/7664118890435218708"
+    },
+    {
+        title: "chackie chan him self",
+        description: "Daily movie news and cinematic moments.",
+        tiktok: "https://www.tiktok.com/@moviepulse_247/video/7665768149773503764"
+    },
+     {
+        title: "khala and the sun",
+        description: "Daily movie news and cinematic moments.",
+        tiktok: "https://www.tiktok.com/@moviepulse_247/video/7664493847560375573"
+    },
+      {
+        title: "he wasn't ready for this",
+        description: "😍there he is",
+        tiktok: "https://www.tiktok.com/@moviepulse_247/video/7664277598419160341"
     }
 ];
