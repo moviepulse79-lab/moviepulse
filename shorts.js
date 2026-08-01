11dc53009157961c1e637f27d33f9b2c70f0504f
MOVIE PULSE <moviepulse79@gmail.com>
2026-08-01 13:31:42 +0200

    Fix TikTok links and add movie IDs

--- shorts.js
+++ shorts.js
@@ -3,26 +3,30 @@ const shorts = [
     {
         title: "Children of Blood and Bone",
         description: "Latest MoviePulse edit and movie update.",
-        tiktok: "https://www.tiktok.com/@moviepulse_247/video/7668646314795732231"
+        tiktok: "https://www.tiktok.com/@moviepulse_247/video/7668646314795732231",
+        movieId:82
     },
 
 
     {
         title: "MayDAY2026",
         description: "A look at one of the most anticipated movies.",
-        tiktok: "https://www.tiktok.com/@moviepulse_247/video/7663152329075592455"
+        tiktok: "https://www.tiktok.com/@moviepulse_247/video/7663152329075592455",
+        movieId:78
     },
 
 
     {
         title: "SPIDER-MAN:Brand new day",
         description: "Daily movie news and cinematic moments.",
-        tiktok: "https://www.tiktok.com/@moviepulse_247/video/7663518101870349575"
+        tiktok: "https://www.tiktok.com/@moviepulse_247/video/7663518101870349575".
+        movieId:35
     },
      {
         title: "The End of Aok street",
         description: "Daily movie news and cinematic moments.",
-        tiktok: "https://www.tiktok.com/@moviepulse_247/video/7658630866230447368"
+        tiktok: "https://www.tiktok.com/@moviepulse_247/video/7658630866230447368",
+         movieId:58
     },
      {
         title: "Priyanka chopra in her Era",
@@ -37,11 +41,13 @@ const shorts = [
      {
         title: "khala and the sun",
         description: "Daily movie news and cinematic moments.",
-        tiktok: "https://www.tiktok.com/@moviepulse_247/video/7664493847560375573"
+        tiktok: "https://www.tiktok.com/@moviepulse_247/video/7664493847560375573",
+        movieId:62
     },
       {
         title: "he wasn't ready for this",
         description: "😍there he is",
-        tiktok: "https://www.tiktok.com/@moviepulse_247/video/7664277598419160341"
+        tiktok: "https://www.tiktok.com/@moviepulse_247/video/7664277598419160341",
+          movieId:35
     }
 ];
