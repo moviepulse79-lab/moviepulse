37985470a0d145d658fd91acf9d893cbf290c9f0
MOVIE PULSE <moviepulse79@gmail.com>
2026-08-01 13:52:53 +0200

    Update shorts.js

--- shorts.js
+++ shorts.js
@@ -4,6 +4,7 @@ const shorts = [
         title: "Children of Blood and Bone",
         description: "Latest MoviePulse edit and movie update.",
         tiktok: "https://www.tiktok.com/@moviepulse_247/video/7668646314795732231",
+        category:"fantasy",
         movieId:82
     },
 
@@ -12,6 +13,7 @@ const shorts = [
         title: "MayDAY2026",
         description: "A look at one of the most anticipated movies.",
         tiktok: "https://www.tiktok.com/@moviepulse_247/video/7663152329075592455",
+        category:"action",
         movieId:78
     },
 
@@ -20,34 +22,40 @@ const shorts = [
         title: "SPIDER-MAN:Brand new day",
         description: "Daily movie news and cinematic moments.",
         tiktok: "https://www.tiktok.com/@moviepulse_247/video/7663518101870349575",
+        category:"superhero",
         movieId:35
     },
      {
         title: "The End of Aok street",
         description: "Daily movie news and cinematic moments.",
         tiktok: "https://www.tiktok.com/@moviepulse_247/video/7658630866230447368",
+         category:"horror",
          movieId:58
     },
      {
         title: "Priyanka chopra in her Era",
         description: "Daily movie news and cinematic moments.",
-        tiktok: "https://www.tiktok.com/@moviepulse_247/video/7664118890435218708"
+        tiktok: "https://www.tiktok.com/@moviepulse_247/video/7664118890435218708",
+         category:"action"
     },
     {
         title: "chackie chan him self",
         description: "Daily movie news and cinematic moments.",
-        tiktok: "https://www.tiktok.com/@moviepulse_247/video/7665768149773503764"
+        tiktok: "https://www.tiktok.com/@moviepulse_247/video/7665768149773503764",
+        category:"action"
     },
      {
         title: "khala and the sun",
         description: "Daily movie news and cinematic moments.",
         tiktok: "https://www.tiktok.com/@moviepulse_247/video/7664493847560375573",
+         category:"drama",
         movieId:62
     },
       {
         title: "he wasn't ready for this",
         description: "😍there he is",
         tiktok: "https://www.tiktok.com/@moviepulse_247/video/7664277598419160341",
+          category:"superhero",
           movieId:35
     }
 ];
