7f86e709f60f7e3eef2bfa8773fcacfcccb6c402
MOVIE PULSE <moviepulse79@gmail.com>
2026-08-02 17:29:21 +0200

    Update favorite.js

--- favorite.js
+++ favorite.js
@@ -1,48 +1,29 @@
-const favoritesContainer =
-document.getElementById("favoritesContainer");
-
-
-let favorites =
-JSON.parse(localStorage.getItem("favorites")) || [];
-
-
-let favoriteMovies = movies.filter(movie =>
-    favorites.includes(movie.id)
-);
-
-
-if(favoriteMovies.length === 0){
-
-    favoritesContainer.innerHTML = `
-    <h2>No favorites yet ❤️</h2>
-    <p>Open a movie and add it to your favorites.</p>
-    `;
-
-}else{
-
-
 favoriteMovies.forEach(movie => {
-
 
 favoritesContainer.innerHTML += `
 
 <div class="movie-card">
 
-<a href="trailer.html?id=${movie.id}">
+    <div class="poster-box">
 
-<img src="${movie.poster}" alt="${movie.title}">
+        <button class="remove-favorite" onclick="removeFavorite(${movie.id})">
+            ❤️
+        </button>
 
-</a>
+        <a href="trailer.html?id=${movie.id}">
+            <img src="${movie.poster}" alt="${movie.title}">
+        </a>
 
-<h3>${movie.title}</h3>
+    </div>
 
-<span>⭐ ${movie.rating}</span>
+    <h3>${movie.title}</h3>
+
+    <div class="movie-rating">
+        ⭐ ${movie.rating}
+    </div>
 
 </div>
 
 `;
 
 });
-
-
-}
