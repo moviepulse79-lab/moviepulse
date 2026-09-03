4fbb719e84f5cf23b34b15fe2870f12aef28409e
MOVIE PULSE <moviepulse79@gmail.com>
2026-09-03 18:25:57 +0200

    Update vimeo-movies.js

--- vimeo-movies.js
+++ vimeo-movies.js
@@ -13,31 +13,9 @@ const vimeoMovies = [
             "A terminally ill parking enforcement officer wants nothing more than to live out his days in peace, but his family and friends have other plans."
     },
 
-    {
-        id: "okie-down",
-        title: "OKIE DOWN",
-        year: 2015,
-        genre: "Drama / Thriller / Noir",
-        duration: "1h 38min",
-        rating: "6.5",
-        poster: "https://i.vimeocdn.com/video/145389729_640.jpg",
-        vimeoId: "145389729",
-        description:
-            "A noir psychological suspense drama set in a small American Midwest town where domestic problems spiral into a murder mystery."
-    },
 
-    {
-        id: "box-full-of-spiders",
-        title: "A Box Full of Spiders",
-        year: 2023,
-        genre: "Horror",
-        duration: "1h 16min",
-        rating: "6.0",
-        poster: "https://i.vimeocdn.com/video/894745028_640.jpg",
-        vimeoId: "894745028",
-        description:
-            "Three college girls become trapped inside their sorority house and face a terrifying nightmare involving masked killers and supernatural evil."
-    },
+
+
 
     {
         id: "minds-eye",
@@ -63,45 +41,7 @@ const vimeoMovies = [
         vimeoId: "46777125",
         description:
             "An award-winning Canadian feature film following a man caught in the aftermath of a devastating event."
-    },
-
-    {
-        id: "theox",
-        title: "THEOX",
-        year: 2017,
-        genre: "Animation / Drama",
-        duration: "Feature Film",
-        rating: "6.0",
-        poster: "https://i.vimeocdn.com/video/231306317_640.jpg",
-        vimeoId: "231306317",
-        description:
-            "A live-action animation feature exploring obedience, power and the possibility of change."
-    },
-
-    {
-        id: "rainbowmaker",
-        title: "The Rainbowmaker",
-        year: 2008,
-        genre: "Drama / Romance",
-        duration: "Feature Film",
-        rating: "6.5",
-        poster: "https://i.vimeocdn.com/video/448835175_640.jpg",
-        vimeoId: "448835175",
-        description:
-            "A man returns home after prison and discovers his family has moved on without him, leading him on a journey to reclaim his place in their lives."
-    },
-
-    {
-        id: "lost-in-carranza",
-        title: "Lost in Carranza",
-        year: 2019,
-        genre: "Documentary / Drama",
-        duration: "Feature Film",
-        rating: "7.0",
-        poster: "https://i.vimeocdn.com/video/392459996_640.jpg",
-        vimeoId: "392459996",
-        description:
-            "An independent documentary following Pablo Carranza as he struggles with addiction, isolation and the difficult realities of his life."
     }
 
+ 
 ];
