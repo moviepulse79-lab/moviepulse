a9e087db7a1531d83176a1999cef9d164c94ab3b
MOVIE PULSE <moviepulse79@gmail.com>
2026-09-03 16:41:50 +0200

    Add vimeoMovies array with movie details

---
+++ vimeo-movies.js
@@ -0,0 +1,14 @@
+const vimeoMovies = [
+    {
+        id: "time-expired",
+        title: "Time Expired",
+        year: 2011,
+        genre: "Comedy / Drama",
+        duration: "1h 33min",
+        rating: "7.0",
+        poster: "https://i.vimeocdn.com/video/237605099_640.jpg",
+        vimeoId: "32835730",
+        description:
+            "A terminally ill parking enforcement officer wants nothing more than to live out his days in peace, but his family and friends have other plans."
+    }
+];
