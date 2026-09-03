14ec2154449f372490674aff3d04a475526f8a1d
MOVIE PULSE <moviepulse79@gmail.com>
2026-09-03 18:09:21 +0200

    Update vimeo-movies.js

--- vimeo-movies.js
+++ vimeo-movies.js
@@ -11,19 +11,8 @@ const vimeoMovies = [
         vimeoId: "32835730",
         description:
             "A terminally ill parking enforcement officer wants nothing more than to live out his days in peace, but his family and friends have other plans."
-    },
-
-    {
-        id: "okie-down",
-        title: "OKIE DOWN",
-        year: 2022,
-        genre: "Drama",
-        duration: "1h 38min",
-        rating: "7.0",
-        poster: "",
-        vimeoId: "145389729",
-        description:
-            "A full-length independent feature film available to watch on Vimeo."
     }
+
+
 
 ];
